9db0e7d9307b541ee1505f2376cbd65716e69a1b
atis112 <toth.ati13@gmail.com>
2012-09-20 12:53:53 +0200

    Update for kumquat

--- prebuilt/hw_config.sh
+++ prebuilt/hw_config.sh
@@ -1,22 +1,21 @@
-# HW configuration file for nypon
-
-# Proximity sensor configuration (NOA3402)
-dev=/sys/devices/platform/nmk-i2c.2/i2c-2/2-0037
-echo 1300 0e00 > $dev/ps_threshold # Hysteresis thershold values in hex (<hi> <lo>)
-
-display_id=`cat /sys/devices/mcde_display_panel.0/ddb_id`
-case "$display_id" in
-        "021e8423")
-                fw=incell_fw_120hz.img
-        ;;
-        *)
-                fw=incell_fw_default.img
-        ;;
+# HW configuration file for kumquat
+# Touch FW loader
+dev=/sys/bus/spi/devices/spi9.0
+fw=ttsp_fw.hex
+app_id=`cat  $dev/appid`
+case `cat /data/ttsp_fw_update` in
+        "in_progress") flags=-force ;;
+        *) flags="" ;;
 esac
-echo $display_id
-echo $fw
+case "$app_id" in
+	"0x3030") flags=-force ;;
+	*) echo $app_id > /data/ttsp_appid ;;
+esac
 
-semc_rmi4_fwloader -b /system/etc/firmware/$fw -d /sys/bus/rmi4/devices/sensor00
+echo "in_progress" > /data/ttsp_fw_update
+cyttsp_fwloader -dev $dev -fw /system/etc/firmware/$fw $flags
+echo "done" > /data/ttsp_fw_update
+
 
 # Audio jack configuration
 dev=/sys/devices/platform/simple_remote.0
@@ -24,3 +23,17 @@ echo 0,301,1901 > $dev/accessory_min_vals
 echo 300,1900  > $dev/accessory_max_vals
 echo 0,51,251,511,851 > $dev/button_min_vals
 echo 50,250,510,850,5000  > $dev/button_max_vals
+
+# Proximity sensor configuration
+dev=/sys/bus/i2c/devices/2-0054/
+val_cycle=2
+val_nburst=0
+val_freq=2
+val_threshold=5
+val_filter=0
+
+echo $val_cycle > $dev/cycle    # Duration Cycle. Valid range is 0 - 3.
+echo $val_nburst > $dev/nburst  # Numb er of pulses in burst. Valid range is 0 - 15.
+echo $val_freq > $dev/freq      # Burst frequency. Valid range is 0 - 3.
+echo $val_threshold > $dev/threshold # sensor threshold. Valid range is 0 - 15 (0.12V - 0.87V)
+echo $val_filter > $dev/filter  # RFilter. Valid range is 0 - 3.
